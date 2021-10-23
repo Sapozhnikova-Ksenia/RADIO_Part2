5b40b3577fe5468a7b06468b180a3a32e6cee7a2
Sapozhnikova Ksenia <mobillka92@gmail.com>
2021-10-23 20:53:40 +0300

    flexible

--- src/main/java/ru/netology/radioman/Radio.java
+++ src/main/java/ru/netology/radioman/Radio.java
@@ -5,8 +5,57 @@ public class Radio {
     private int lastRadiostation = 9;
     private int currentRadiostation;
     private int minVolume = 0;
-    private int maxVolume = 10;
+    private int maxVolume = 100;
     private int currentVolume;
+    private  int quantityRadiostation= 10;
+
+    public Radio() {
+    }
+
+    public Radio (int quantityRadiostation){
+        this.quantityRadiostation= quantityRadiostation;
+        this.lastRadiostation= this.quantityRadiostation-1;
+    }
+
+    public int getFirstRadiostation() {
+        return firstRadiostation;
+    }
+
+    public void setFirstRadiostation(int firstRadiostation) {
+        this.firstRadiostation = firstRadiostation;
+    }
+
+    public int getLastRadiostation() {
+        return lastRadiostation;
+    }
+
+    public void setLastRadiostation(int lastRadiostation) {
+        this.lastRadiostation = lastRadiostation;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getQuantityRadiostation() {
+        return quantityRadiostation;
+    }
+
+    public void setQuantityRadiostation(int quantityRadiostation) {
+        this.quantityRadiostation = quantityRadiostation;
+    }
 
     public int getCurrentVolume() {
         return currentVolume;
